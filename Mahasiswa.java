35922b9b5fabe2d777bcdb8ed510d118bebeee30
rachel.sausan <rachelsausan9g@gmail.com>
2018-09-07 14:12:44 +0700

    Initial Commit Mahasiswa.Java

---
+++ Mahasiswa.java
@@ -0,0 +1,27 @@
+
+public class Mahasiswa {
+	private String name;
+    private String npm;
+    
+    // ini method setter
+    public void setName(String name){
+    	
+    	this.name = name;
+    	
+    }
+    
+    public void setNpm(String npm){
+    	this.npm = npm;
+	}
+    
+    // ini method getter
+    public String getName(){
+    	return this.name;
+    }
+    
+    public String getNpm(){
+    	return this.npm;
+	}
+	
+
+}
